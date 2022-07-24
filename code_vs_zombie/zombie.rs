1d33c791a4d7410e7fff3a5b9630310d136b70a7
manu3618 <manu+github@hbrt.eu>
2022-07-24 13:23:21 +0200

    [ZOMBIES] add code from codingame

---
+++ code_vs_zombie/zombie.rs
@@ -0,0 +1,49 @@
+use std::io;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+}
+
+/**
+ * Save humans, destroy zombies!
+ **/
+fn main() {
+
+    // game loop
+    loop {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let inputs = input_line.split(" ").collect::<Vec<_>>();
+        let x = parse_input!(inputs[0], i32);
+        let y = parse_input!(inputs[1], i32);
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let human_count = parse_input!(input_line, i32);
+        for i in 0..human_count as usize {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let inputs = input_line.split(" ").collect::<Vec<_>>();
+            let human_id = parse_input!(inputs[0], i32);
+            let human_x = parse_input!(inputs[1], i32);
+            let human_y = parse_input!(inputs[2], i32);
+        }
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let zombie_count = parse_input!(input_line, i32);
+        for i in 0..zombie_count as usize {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let inputs = input_line.split(" ").collect::<Vec<_>>();
+            let zombie_id = parse_input!(inputs[0], i32);
+            let zombie_x = parse_input!(inputs[1], i32);
+            let zombie_y = parse_input!(inputs[2], i32);
+            let zombie_xnext = parse_input!(inputs[3], i32);
+            let zombie_ynext = parse_input!(inputs[4], i32);
+        }
+
+        // Write an action using println!("message...");
+        // To debug: eprintln!("Debug message...");
+
+        println!("0 0"); // Your destination coordinates
+    }
+}
