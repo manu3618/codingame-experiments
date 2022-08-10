a8f15b9949abe363b48bd5beb7a547f020d52f20
manu3618 <manu+github@hbrt.eu>
2022-08-10 17:05:44 +0200

    [MARS LANDER] Rename episode

--- mars_lander/lander_episode1.rs
+++ mars_lander/lander_episode1.rs
@@ -1,19 +1,20 @@
-// https://www.codingame.com/ide/puzzle/mars-lander
 use std::io;
 
 macro_rules! parse_input {
-    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+    ($x:expr, $t:ident) => {
+        $x.trim().parse::<$t>().unwrap()
+    };
 }
 
 /**
- * Save the Planet.
- * Use less Fossil Fuel.
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
  **/
 fn main() {
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
-    let n = parse_input!(input_line, i32); // the number of points used to draw the surface of Mars.
-    for i in 0..n as usize {
+    let surface_n = parse_input!(input_line, i32); // the number of points used to draw the surface of Mars.
+    for i in 0..surface_n as usize {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let inputs = input_line.split(" ").collect::<Vec<_>>();
@@ -28,17 +29,16 @@ fn main() {
         let inputs = input_line.split(" ").collect::<Vec<_>>();
         let x = parse_input!(inputs[0], i32);
         let y = parse_input!(inputs[1], i32);
-        let hs = parse_input!(inputs[2], i32); // the horizontal speed (in m/s), can be negative.
-        let vs = parse_input!(inputs[3], i32); // the vertical speed (in m/s), can be negative.
-        let f = parse_input!(inputs[4], i32); // the quantity of remaining fuel in liters.
-        let r = parse_input!(inputs[5], i32); // the rotation angle in degrees (-90 to 90).
-        let p = parse_input!(inputs[6], i32); // the thrust power (0 to 4).
+        let h_speed = parse_input!(inputs[2], i32); // the horizontal speed (in m/s), can be negative.
+        let v_speed = parse_input!(inputs[3], i32); // the vertical speed (in m/s), can be negative.
+        let fuel = parse_input!(inputs[4], i32); // the quantity of remaining fuel in liters.
+        let rotate = parse_input!(inputs[5], i32); // the rotation angle in degrees (-90 to 90).
+        let power = parse_input!(inputs[6], i32); // the thrust power (0 to 4).
 
         // Write an action using println!("message...");
         // To debug: eprintln!("Debug message...");
 
-
-        // R P. R is the desired rotation angle. P is the desired thrust power.
-        println!("-20 3");
+        // 2 integers: rotate power. rotate is the desired rotation angle (should be 0 for level 1), power is the desired thrust power (0 to 4).
+        println!("0 3");
     }
 }
