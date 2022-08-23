5d93f7ae70351b75bfd9c44ba7d4b01b882c3e66
manu3618 <manu+github@hbrt.eu>
2022-08-23 11:31:10 +0200

    [BRAINFUCK] Add simple cases
    works in simple case, without repetitions

--- brain_fork/brain.rs
+++ brain_fork/brain.rs
@@ -1,6 +1,11 @@
 // https://www.codingame.com/ide/puzzle/brain-fork
 use std::io;
 
+static VALUES: [char; 27] = [
+    ' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+    'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+];
+
 macro_rules! parse_input {
     ($x:expr, $t:ident) => {
         $x.trim().parse::<$t>().unwrap()
@@ -17,16 +22,17 @@ fn main() {
     io::stdin().read_line(&mut input_line).unwrap();
     let magic_phrase = input_line.trim_matches('\n').to_string();
     let letters: Vec<char> = magic_phrase.chars().collect(); // wanted output
-    let mut rune = 0 as char;
+    let mut rune = 'A';
     let mut forest: Vec<char> = Vec::new(); // current status
     let mut output_letter = "".to_string();
 
     // Write an action using println!("message...");
     // To debug: eprintln!("Debug message...");
     for position in 0..letters.len() {
-        let init_value = 0 as char;
+        output_letter += ">";
+        let init_value = ' ';
         output_letter += &get_letter(letters[position], &init_value);
-        output_letter += ".>";
+        output_letter += ".";
     }
 
     println!("{}", output_letter);
@@ -35,11 +41,15 @@ fn main() {
 /// Get part of string needed to obtain the new letter
 /// run is modified in place
 fn get_letter(wanted_letter: char, rune: &char) -> String {
-    if *rune as u32 > wanted_letter as u32 {
-        let diff = *rune as u32 - wanted_letter as u32;
+    eprintln!("{}", *rune);
+    let rune_pos = VALUES.iter().position(|&x| x == *rune).unwrap();
+    let wanted_pos = VALUES.iter().position(|&x| x == wanted_letter).unwrap();
+
+    if rune_pos > wanted_pos {
+        let diff = rune_pos - wanted_pos;
         return "-".repeat(diff as usize);
     } else {
-        let diff = wanted_letter as u32 - *rune as u32;
+        let diff = wanted_pos - rune_pos;
         return "+".repeat(diff as usize);
     }
 }
