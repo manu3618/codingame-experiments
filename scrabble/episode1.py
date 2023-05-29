b0b87bc307f6d5a536a88ea326ac538c34db3d26
manu3618 <manu+github@hbrt.eu>
2023-05-29 16:35:29 +0200

    [SCRABBLE] Add code from codingame

---
+++ scrabble/episode1.py
@@ -0,0 +1,16 @@
+# https://www.codingame.com/ide/puzzle/scrabble
+import math
+import sys
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+for i in range(n):
+    w = input()
+letters = input()
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print("invalid word")
