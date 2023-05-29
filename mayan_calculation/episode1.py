c1e441f3a34cf6bae8e1dd481cec8a846bdb059d
manu3618 <manu+github@hbrt.eu>
2023-05-29 15:22:34 +0200

    [MAYAN] Add initial file

---
+++ mayan_calculation/episode1.py
@@ -0,0 +1,22 @@
+# https://www.codingame.com/ide/puzzle/mayan-calculation
+import math
+import sys
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+l, h = [int(i) for i in input().split()]
+for i in range(h):
+    numeral = input()
+s1 = int(input())
+for i in range(s1):
+    num_1line = input()
+s2 = int(input())
+for i in range(s2):
+    num_2line = input()
+operation = input()
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print("result")
