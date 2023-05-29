a9924bbfe74716325a45108021f4ce04b95d9f70
manu3618 <manu+github@hbrt.eu>
2023-05-29 17:29:29 +0200

    [SCRABBLE] Add functional solution

--- scrabble/episode1.py
+++ scrabble/episode1.py
@@ -1,16 +1,47 @@
 # https://www.codingame.com/ide/puzzle/scrabble
-import math
 import sys
+from collections import Counter
+from itertools import permutations
 
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
+# points
+pts = [
+    (("eaionrtlsu"), 1),
+    (("dg"), 2),
+    (("bcmp"), 3),
+    (("fhvwy"), 4),
+    (("k"), 5),
+    (("jx"), 6),
+    (("qz"), 10),
+]
+points = {}
+for letters, point in pts:
+    for c in letters:
+        points[c] = point
 
+
+# dictionnary
 n = int(input())
+words = []
 for i in range(n):
-    w = input()
-letters = input()
+    words.append(input())
 
-# Write an answer using print
-# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+# letters
+letters = list(input())
 
-print("invalid word")
+
+# solver
+valid_words = Counter()
+attempts = (
+    w
+    for k in range(1, len(letters) + 1)
+    for le in permutations(letters, k)
+    if (w := "".join(le)) in words
+)
+
+for attempt in attempts:
+    valid_words[attempt] = sum(points[c] for c in attempt)
+
+max_score = valid_words.most_common(1)[0][1]
+answers = [k for k, v in valid_words.items() if v == max_score]
+
+print(next(w for w in words if w in answers))
