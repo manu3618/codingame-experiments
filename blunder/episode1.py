f6591378735546e24866ecb96c6cc066adb4c747
manu3618 <manu+github@hbrt.eu>
2023-05-29 19:17:24 +0200

    [Blunder] Add code from codingame

---
+++ blunder/episode1.py
@@ -0,0 +1,15 @@
+# https://www.codingame.com/ide/puzzle/blunder-episode-1
+import math
+import sys
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+l, c = [int(i) for i in input().split()]
+for i in range(l):
+    row = input()
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print("answer")
