ae4d3c7849cd057b78da9ec1f7725695be952aa2
manu3618 <manu+github@hbrt.eu>
2023-06-06 18:02:57 +0200

    [PHONE NUMBER] WIP on perf improvments

--- phone_num/episode1.py
+++ phone_num/episode1.py
@@ -1,7 +1,6 @@
 # https://www.codingame.com/ide/puzzle/telephone-numbers
 import math
 import sys
-from itertools import product
 
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
@@ -10,25 +9,30 @@ phone_nums = []
 
 n = int(input())
 storage = 0
+to_break = False
+
+
+def get_max_common_len(a: str, b: str):
+    for idx in range(len(a) + 1, 0, -1):
+        if a[:idx] == b[:idx]:
+            return idx
+    return 0
+
+
 for _ in range(n):
     telephone = input()
-    max_common_len = 0
     if any(telephone.startswith(x) or x.startswith(telephone) for x in phone_nums):
-        # no more storage
         phone_nums.append(telephone)
         continue
-    common_lengths = {
-        idx
-        for idx, num in product(range(len(telephone)), phone_nums)
-        if telephone.startswith(num[:idx])
-    }
-    common_lengths.add(0)
-    storage += len(telephone) - max(common_lengths)
+
+    c = [get_max_common_len(num, telephone) for num in phone_nums]
+    c.append(0)
+    m = max(c)
+
+    print("Debug messages... to add ", m, file=sys.stderr, flush=True)
+    storage += len(telephone) - m
+    print("Debug messages... current", storage, file=sys.stderr, flush=True)
     phone_nums.append(telephone)
 
 
-# Write an answer using print
-# To debug: print("Debug messages...", file=sys.stderr, flush=True)
-
-# The number of elements (referencing a number) stored in the structure.
 print(storage)
