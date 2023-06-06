c6cc826597177452e48a5e5367ccaf311bc3ee13
manu3618 <manu+github@hbrt.eu>
2023-06-06 16:52:49 +0200

    [GIANT] Working solution for episode1

--- giant/episode1.py
+++ giant/episode1.py
@@ -1,18 +1,36 @@
 # https://www.codingame.com/ide/puzzle/dwarfs-standing-on-the-shoulders-of-giants
-import math
 import sys
+from itertools import product
 
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
+paths = []
+
+
+def update_path(paths, x, y):
+    """Add y to each path finishing by x"""
+    for path in paths:
+        print("path", path, file=sys.stderr)
+        if path[-1] == x:
+            print("path", path, y, file=sys.stderr)
+            paths.append(path + [y])
+        if path[0] == y:
+            print("path", x, path, file=sys.stderr)
+            paths.append([x] + path)
+    paths.append([x, y])
+
+
+def join_disjoints(paths):
+    for path0, path1 in product(paths, paths):
+        if path0[-1] == path1[0]:
+            print("joining", path0, path1, file=sys.stderr)
+            paths.append(path0 + path1[1:])
+
 
 n = int(input())  # the number of relationships of influence
-for i in range(n):
-    # x: a relationship of influence between two people (x influences y)
-    x, y = [int(j) for j in input().split()]
+for _ in range(n):
+    x, y = [int(x) for x in input().split()]
+    print("____", x, y, "____", file=sys.stderr)
+    update_path(paths, x, y)
 
-# Write an answer using print
-# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+join_disjoints(paths)
 
-
-# The number of people involved in the longest succession of influences
-print("2")
+print(max(len(path) for path in paths))
