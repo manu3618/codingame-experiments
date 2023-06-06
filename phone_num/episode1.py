e71a07ebd2e167c76ec26c4bced0b08d10889550
manu3618 <manu+github@hbrt.eu>
2023-06-06 16:57:15 +0200

    [PHONE NUM] Add code from codingame

---
+++ phone_num/episode1.py
@@ -0,0 +1,16 @@
+# https://www.codingame.com/ide/puzzle/telephone-numbers
+import math
+import sys
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+for i in range(n):
+    telephone = input()
+
+    # Write an answer using print
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+    # The number of elements (referencing a number) stored in the structure.
+    print("number")
