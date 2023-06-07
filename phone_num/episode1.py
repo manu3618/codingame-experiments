8e8e13a1c90d433cfb2c5c3e24905209fec38ccb
manu3618 <manu+github@hbrt.eu>
2023-06-07 15:53:08 +0200

    [PHONE NUMBER] Use common prefixes instead of phone numbers

--- phone_num/episode1.py
+++ phone_num/episode1.py
@@ -2,31 +2,21 @@
 import math
 import sys
 
-phone_nums = set()
+prefixes = set()
 
 n = int(input())
 storage = 0
 
 
-def get_max_common_len(a: str, b: str):
-    for idx in range(len(a) + 1, 0, -1):
-        if a[:idx] == b[:idx]:
-            return idx
-    return 0
-
-
-def get_additional_storage(num: str, phone_numbers):
-    for idx in range(len(num) + 1, 0, -1):
-        for phone in phone_numbers:
-            if num[:idx] == phone[:idx]:
-                return len(num) - idx
-    return len(num)
-
-
 for _ in range(n):
     telephone = input()
-    storage += get_additional_storage(telephone, phone_nums)
-    phone_nums.add(telephone)
+    new_pref = {
+        y
+        for idx in range(1, len(telephone) + 1)
+        if (y := telephone[:idx]) not in prefixes
+    }
+    storage += len(new_pref)
+    prefixes.update(new_pref)
 
 
 print(storage)
