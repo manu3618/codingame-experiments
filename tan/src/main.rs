0f6006480d7fcc44d7eef325290ecb9151997841
manu3618 <manu+github@hbrt.eu>
2023-09-15 18:05:23 +0200

    [TAN] Implement first path finding

--- tan/src/main.rs
+++ tan/src/main.rs
@@ -1,4 +1,5 @@
 // https://www.codingame.com/ide/puzzle/tan-network
+use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::io;
@@ -68,6 +69,34 @@ struct Link {
     distance: f64,
 }
 
+#[derive(Debug, PartialEq, Default, Clone)]
+struct Route {
+    route: Vec<String>, // stops ids in order
+    distance: f64,
+}
+
+impl Route {
+    fn add_link(&mut self, link: &Link) {
+        if self.route.is_empty() {
+            self.route.push(link.src.clone());
+        }
+        if self.route.contains(&link.dst) {
+            return;
+        }
+        if self.route.last().expect("route contains at least source") != &link.src {
+            return;
+        }
+        self.route.push(link.dst.clone());
+        self.distance += link.distance;
+    }
+}
+
+impl PartialOrd for Route {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.distance.partial_cmp(&other.distance)
+    }
+}
+
 impl Network {
     fn add_link(&mut self, src: &str, dst: &str) {
         let src_stop = self
@@ -88,8 +117,80 @@ impl Network {
             destinations.push(link);
         }
     }
+
     fn insert_stop(&mut self, stop: Stop) {
         self.stops.insert(stop.id.clone(), stop);
+    }
+
+    /// Return all possible route with exactly one more hope.
+    fn extend_route(&self, route: Route) -> Vec<Route> {
+        let mut routes = Vec::new();
+        if route.route.is_empty() {
+            return routes;
+        }
+        let src = route.route.last().expect("route not empty").clone();
+        if let Some(links) = self.links.get(&src) {
+            for link in links {
+                let mut new_route = route.clone();
+                new_route.add_link(link);
+                routes.push(new_route);
+            }
+        }
+        return routes;
+    }
+
+    /// find all routes from src_id to dst_id
+    fn build_routes(&self, src_id: &str, dst_id: &str) -> Vec<Route> {
+        if src_id == dst_id {
+            return vec![Route {
+                route: vec![String::from(src_id)],
+                distance: 0.0,
+            }];
+        }
+        let mut route_modified = true; // whether a route has been modified
+        let mut routes = Vec::new();
+        if let Some(links) = self.links.get(src_id) {
+            for link in links {
+                let mut route = Route::default();
+                route.add_link(&link);
+                routes.push(route);
+            }
+        }
+        let mut new_routes = Vec::new();
+        while route_modified {
+            route_modified = false;
+            for route in &routes {
+                if route.route.last().expect("route is not empty") == dst_id {
+                    // destination reached
+                    continue;
+                }
+                let new_hopes = self.extend_route(route.clone());
+                if new_hopes.is_empty() {
+                    // no new route
+                    continue;
+                }
+                route_modified = true;
+                // dbg!(&new_hopes);
+                new_routes.extend(new_hopes);
+            }
+            routes.append(&mut new_routes);
+        }
+        routes.retain(|r| !r.route.is_empty() && r.route.last().unwrap() == dst_id);
+        return routes;
+    }
+
+    /// get the answer to write.
+    fn get_answer(&self, src_id: &str, dst_id: &str) -> String {
+        let mut routes = self.build_routes(src_id, dst_id);
+        if routes.is_empty() {
+            return "IMPOSSIBLE".into();
+        }
+        routes.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        let mut human_readable = Vec::new();
+        for stop_id in &routes[0].route {
+            human_readable.push(self.stops.get(stop_id).unwrap().name.clone());
+        }
+        return human_readable.join("\n");
     }
 }
 
@@ -126,9 +227,10 @@ fn main() {
         network.add_link(link[0], link[1]);
     }
 
-    dbg!(network);
+    // dbg!(&network);
     // Write an answer using println!("message...");
     // To debug: eprintln!("Debug message...");
-
-    println!("IMPOSSIBLE");
+    // let routes = network.build_routes(&start_point, &end_point);
+    // dbg!(&routes);
+    println!("{}", network.get_answer(&start_point, &end_point));
 }
