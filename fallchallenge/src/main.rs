ec539e1f677e00deb64e1c8cb308fb7eb83fe74d
manu3618 <manu+github@hbrt.eu>
2023-12-24 18:39:35 +0100

    [FALL] Add first solution

--- fallchallenge/src/main.rs
+++ fallchallenge/src/main.rs
@@ -1,5 +1,6 @@
 // https://www.codingame.com/ide/challenge/fall-challenge-2023
 use itertools::iproduct;
+use rand::Rng;
 use std::collections::HashMap;
 use std::io;
 use std::str::FromStr;
@@ -22,6 +23,9 @@ impl Drone {
     fn creature_distance(&self, other: &Creature) -> u32 {
         distance(self.position, other.position)
     }
+    fn distance(&self, position: (u32, u32)) -> u32 {
+        distance(self.position, position)
+    }
 }
 
 #[derive(Debug)]
@@ -30,7 +34,7 @@ struct ParseDroneError;
 impl FromStr for Drone {
     type Err = ParseDroneError;
     fn from_str(s: &str) -> Result<Drone, ParseDroneError> {
-        let fields = s.trim().split(" ").collect::<Vec<_>>();
+        let fields = s.trim().split(' ').collect::<Vec<_>>();
         if fields.len() != 5 {
             return Err(ParseDroneError);
         }
@@ -63,7 +67,7 @@ impl FromStr for Drone {
     }
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 struct Creature {
     id: u8,
     creature_type: u8,
@@ -73,19 +77,29 @@ struct Creature {
 }
 
 impl Creature {
-    fn set_id(self, id: u8) -> Self {
-        Self { id: id, ..self }
-    }
-    fn set_type(self, creature_type: u8) -> Self {
+    /// combine self id, type and color with other position and velocity
+    fn combine(&self, other: &Creature) -> Self {
         Self {
-            creature_type: creature_type,
-            ..self
+            id: self.id,
+            creature_type: self.creature_type.max(other.creature_type),
+            color: self.color.max(other.color),
+            position: other.position,
+            velocity: other.velocity,
         }
     }
-    fn set_color(self, color: u8) -> Self {
+}
+
+impl Default for Creature {
+    fn default() -> Self {
         Self {
-            color: color,
-            ..self
+            id: 0,
+            creature_type: 0,
+            color: 0,
+            position: (
+                rand::thread_rng().gen_range(1..=10000),
+                rand::thread_rng().gen_range(1..=10000),
+            ),
+            velocity: (0, 0),
         }
     }
 }
@@ -96,7 +110,7 @@ struct ParseCreatureError;
 impl FromStr for Creature {
     type Err = ParseCreatureError;
     fn from_str(s: &str) -> Result<Creature, ParseCreatureError> {
-        let fields = s.trim().split(" ").collect::<Vec<_>>();
+        let fields = s.trim().split(' ').collect::<Vec<_>>();
         if fields.len() != 3 && fields.len() != 5 {
             return Err(ParseCreatureError);
         }
@@ -155,14 +169,84 @@ struct Player {
 }
 
 impl Player {
-    /// get position of nearest creature.
+    /// get of nearest creature.
+    /// Returns Some(drone id, creature id)
     fn get_nearest_unscanned_creature(
         &self,
         creatures: &HashMap<u8, Creature>,
-    ) -> Option<(i32, i32)> {
-        // creatures.filter_map(|c| if &self.creatures.contains(c.id){None} else {distance(&self.position)}})
+    ) -> Option<(u8, u8)> {
+        get_nearest_drone_creature(
+            self.drones.values(),
+            creatures.values(),
+            self.creatures.clone(),
+        )
+        .map(|(a, b, _)| (a, b))
+    }
+
+    /// Get nearest creature.
+    /// If creature is in scanning range but not scannable, it must have moved.
+    /// Randomly update those creatures.
+    fn get_update_nearest_creature(
+        &self,
+        creatures: &mut HashMap<u8, Creature>,
+    ) -> Option<(u8, u8)> {
+        let threshold = 500;
+        let mut max_iter = 10;
+        let mut nearest = get_nearest_drone_creature(
+            self.drones.values(),
+            creatures.values(),
+            self.creatures.clone(),
+        );
+
+        while let Some(n) = nearest {
+            if n.2 > threshold {
+                return Some((n.0, n.1));
+            } else {
+                dbg!(&creatures[&n.1]);
+                if let Some(c) = creatures.get_mut(&n.1) {
+                    c.position = (
+                        rand::thread_rng().gen_range(1..=10000),
+                        rand::thread_rng().gen_range(1..=10000),
+                    );
+                }
+                dbg!(&creatures[&n.1]);
+                nearest = get_nearest_drone_creature(
+                    self.drones.values(),
+                    creatures.values(),
+                    self.creatures.clone(),
+                );
+            }
+            if max_iter < 1 {
+                return None;
+            }
+            max_iter -= 1;
+        }
         None
     }
+}
+
+fn get_nearest_drone_creature<'a, D, C>(
+    drones: D,
+    creatures: C,
+    excluded: Vec<u8>,
+) -> Option<(u8, u8, u32)>
+where
+    D: Iterator<Item = &'a Drone>,
+    C: Iterator<Item = &'a Creature>,
+    C: Clone,
+{
+    iproduct!(drones, creatures)
+        .filter_map(|(d, c)| {
+            if excluded.contains(&c.id) {
+                None
+            } else {
+                Some((d.id, c.id, d.creature_distance(c)))
+            }
+        })
+        .collect::<Vec<_>>()
+        .iter()
+        .min_by_key(|e| e.2)
+        .copied()
 }
 
 fn parse_game_input(me: &mut Player, foe: &mut Player, creatures: &mut HashMap<u8, Creature>) {
@@ -203,7 +287,7 @@ fn parse_game_input(me: &mut Player, foe: &mut Player, creatures: &mut HashMap<u
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let foe_drone_count = parse_input!(input_line, usize);
-    for _ in 0..foe_drone_count as usize {
+    for _ in 0..foe_drone_count {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let drone: Drone = input_line.parse().unwrap();
@@ -224,11 +308,18 @@ fn parse_game_input(me: &mut Player, foe: &mut Player, creatures: &mut HashMap<u
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let creature: Creature = input_line.parse().unwrap();
-        creatures.insert(creature.id, creature);
+        let cid = creature.id;
+        creatures
+            .entry(cid)
+            .and_modify(|e| {
+                e.combine(&creature);
+            })
+            .or_insert(creature);
+        // dbg!(&creatures[&cid]);
     }
-    dbg!(&me);
-    dbg!(&foe);
-    dbg!(&creatures);
+    // dbg!(&me);
+    // dbg!(&foe);
+    // dbg!(&creatures);
 }
 
 /**
@@ -251,22 +342,40 @@ fn main() {
     // game loop
     loop {
         parse_game_input(&mut me, &mut foe, &mut creatures);
+        let nearest = me
+            .get_update_nearest_creature(&mut creatures)
+            .map(|(_, n)| n);
+
+        // To ignore
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let radar_blip_count = parse_input!(input_line, i32);
-        for i in 0..radar_blip_count as usize {
+        for _i in 0..radar_blip_count as usize {
             let mut input_line = String::new();
             io::stdin().read_line(&mut input_line).unwrap();
-            let inputs = input_line.split(" ").collect::<Vec<_>>();
-            let drone_id = parse_input!(inputs[0], i32);
-            let creature_id = parse_input!(inputs[1], i32);
-            let radar = inputs[2].trim().to_string();
+            let inputs = input_line.split(' ').collect::<Vec<_>>();
+            let _drone_id = parse_input!(inputs[0], i32);
+            let _creature_id = parse_input!(inputs[1], i32);
+            let _radar = inputs[2].trim().to_string();
         }
-        for i in 0..me.drones.len() {
+
+        for d in me.drones.values() {
             // Write an action using println!("message...");
             // To debug: eprintln!("Debug message...");
-
-            println!("WAIT 1"); // MOVE <x> <y> <light (1|0)> | WAIT <light (1|0)>
+            // println!("WAIT 1"); // MOVE <x> <y> <light (1|0)> | WAIT <light (1|0)>
+            match nearest {
+                Some(cid) => {
+                    let position = creatures[&cid].position;
+                    let light = match d.distance(position) {
+                        0..=2000 => 1,
+                        _ => rand::thread_rng().gen_range(0..=1),
+                    };
+                    println!("MOVE {} {} {}", position.0, position.1, light);
+                }
+                _ => {
+                    println!("WAIT 1");
+                }
+            }
         }
     }
 }
