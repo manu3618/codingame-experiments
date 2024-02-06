a32f547ca5ed7049c33bc9ef6b6dd54553fbd5c4
manu3618 <manu+github@hbrt.eu>
2024-02-06 21:35:08 +0100

    [SURFACE] Add naive algorythm

--- surface/src/main.rs
+++ surface/src/main.rs
@@ -1,4 +1,6 @@
 // https://www.codingame.com/ide/puzzle/surface
+use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fmt;
 use std::io;
 use std::iter::FromIterator;
@@ -14,6 +16,59 @@ struct WaterMap {
     height: usize,
     width: usize,
     map: Vec<Vec<char>>,
+    results: HashMap<(usize, usize), usize>,
+}
+
+impl WaterMap {
+    fn get_neighbors(&self, coords: (usize, usize)) -> HashSet<(usize, usize)> {
+        let mut res = HashSet::with_capacity(4);
+        if coords.0 > 0 {
+            res.insert((coords.0 - 1, coords.1));
+        }
+        if coords.0 < self.width {
+            res.insert((coords.0 + 1, coords.1));
+        }
+        if coords.1 > 0 {
+            res.insert((coords.0, coords.1 - 1));
+        }
+        if coords.1 > self.height {
+            res.insert((coords.0, coords.1 + 1));
+        }
+        res
+    }
+
+    fn get_water_surface(&mut self, coords: (usize, usize)) -> usize {
+        if self.map[coords.0][coords.1] == '#' {
+            return 0;
+        }
+        if let Some(&n) = self.results.get(&coords) {
+            return n;
+        }
+        let mut area = HashSet::new();
+        let mut to_explore = self.get_neighbors(coords);
+        while !to_explore.is_empty() {
+            let mut new_explore = HashSet::new(); // to_explore for next iteration
+            for n in to_explore {
+                // add to area if needed
+                match self.map[n.0][n.1] {
+                    '#' => continue,
+                    'O' => area.insert(n),
+                    _ => unreachable!(),
+                };
+
+                // extend area to explore for next iteration
+                new_explore.extend(self.get_neighbors(n).iter().filter(|c| !area.contains(c)));
+            }
+            to_explore = new_explore;
+        }
+
+        // memoization
+        let result = area.len();
+        for c in area {
+            self.results.insert(c, result);
+        }
+        result
+    }
 }
 
 impl fmt::Display for WaterMap {
@@ -39,12 +94,13 @@ impl FromIterator<String> for WaterMap {
         let width = map.iter().map(|l| l.len()).min().unwrap_or(0);
         WaterMap {
             height: map.len(),
-            width: width,
+            width,
             map: map
                 .iter()
-                .map(|l| (&l[..width].iter().map(|a| *a)).clone().collect::<Vec<_>>())
+                .map(|l| l[..width].iter().copied().clone().collect::<Vec<_>>())
                 .collect::<Vec<_>>()
                 .clone(),
+            results: HashMap::new(),
         }
     }
 }
@@ -67,23 +123,21 @@ fn main() {
         let row = input_line.trim_matches('\n').to_string();
         map_input.push(row);
     }
-    let water_map = WaterMap::from_iter(map_input);
+    let mut water_map = WaterMap::from_iter(map_input);
     eprintln!("map");
-    eprintln!("{}", &water_map); // XXX
+    eprintln!("{}", &water_map);
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let n = parse_input!(input_line, i32);
-    for i in 0..n as usize {
+    for _ in 0..n as usize {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
-        let inputs = input_line.split(" ").collect::<Vec<_>>();
-        let x = parse_input!(inputs[0], i32);
-        let y = parse_input!(inputs[1], i32);
-    }
-    for i in 0..n as usize {
+        let inputs = input_line.split(' ').collect::<Vec<_>>();
+        let x = parse_input!(inputs[0], usize);
+        let y = parse_input!(inputs[1], usize);
+
         // Write an answer using println!("message...");
         // To debug: eprintln!("Debug message...");
-
-        println!("answer");
+        println!("{}", water_map.get_water_surface((x, y)));
     }
 }
