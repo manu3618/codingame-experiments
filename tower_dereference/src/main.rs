65bdd50506b59441dee5bad1f07816b2b73b6019
manu3618 <manu+github@hbrt.eu>
2025-11-29 20:48:37 +0100

    [TOWER DEFENSE] WIP on parsing  #

--- tower_dereference/src/main.rs
+++ tower_dereference/src/main.rs
@@ -1,8 +1,112 @@
 // https://www.codingame.com/ide/puzzle/tower-dereference
+use std::fmt;
 use std::io;
 
 macro_rules! parse_input {
-    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+    ($x:expr, $t:ident) => {
+        $x.trim().parse::<$t>().unwrap()
+    };
+}
+
+#[derive(Default)]
+struct Map(Vec<Vec<char>>);
+
+impl fmt::Display for Map {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for v in &self.0 {
+            write!(f, "{}", v.iter().collect::<String>())?
+        }
+        Ok(())
+    }
+}
+
+impl Map {
+    fn size(&self) -> (usize, usize) {
+        (self.0.len(), self.0.first().map_or(0, |elt| elt.len()))
+    }
+    fn from_stdin() -> Self {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let mut line = input_line.split(" ");
+        let (Some(width), Some(height)) = (line.next(), line.next()) else {
+            panic!("fail to parse map size")
+        };
+        let width: usize = width.parse().unwrap();
+        let height: usize = height.parse().unwrap();
+
+        let mut map = Vec::new();
+        for _ in 0..height {
+            io::stdin().read_line(&mut input_line).unwrap();
+            input_line.trim();
+            map.push(input_line.chars().into_iter().collect::<Vec<_>>());
+            debug_assert!(map.last().unwrap().len() == width);
+        }
+        Self(map.clone())
+    }
+}
+
+#[derive(Default, Debug)]
+struct Player {
+    side: Side,
+    money: usize,
+    live: usize,
+}
+
+#[derive(Default, Debug)]
+enum Side {
+    #[default]
+    Left,
+    Right,
+}
+
+#[derive(Default, Debug)]
+struct Attacker {
+    id: usize,
+    owner: Side,
+    coordinates: (usize, usize),
+    hit_points: u32,
+    max_hit_points: u32,
+    current_speed: u32,
+    max_speed: u32,
+    slow_time: u32,
+    bounty: u32,
+}
+
+impl Attacker {
+    fn from_stdin() -> Self {
+        todo!()
+    }
+}
+
+#[derive(Default, Debug)]
+enum TowerType {
+    #[default]
+    Gun,
+    Fire,
+    Glue,
+    Heal,
+}
+
+#[derive(Default, Debug)]
+struct Tower {
+    id: usize,
+    tower_type: TowerType,
+    owner: Side,
+    coordinates: (usize, usize),
+    damage: usize,
+    range: usize,
+
+    /// number of turns left before being able to fire again
+    cooldown: usize,
+    damage_level: u8,
+    range_level: u8,
+    reload_level: u8,
+}
+
+impl Tower {
+    fn from_stdin() -> Self {
+        todo!()
+    }
 }
 
 /**
@@ -12,16 +116,18 @@ fn main() {
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let player_id = parse_input!(input_line, i32);
-    let mut input_line = String::new();
-    io::stdin().read_line(&mut input_line).unwrap();
-    let inputs = input_line.split(" ").collect::<Vec<_>>();
-    let width = parse_input!(inputs[0], i32);
-    let height = parse_input!(inputs[1], i32);
-    for i in 0..height as usize {
-        let mut input_line = String::new();
-        io::stdin().read_line(&mut input_line).unwrap();
-        let line = input_line.trim_matches('\n').to_string();
-    }
+    let map = Map::from_stdin();
+    dbg!("{map}");
+    // let mut input_line = String::new();
+    // io::stdin().read_line(&mut input_line).unwrap();
+    // let inputs = input_line.split(" ").collect::<Vec<_>>();
+    // let width = parse_input!(inputs[0], i32);
+    // let height = parse_input!(inputs[1], i32);
+    // for i in 0..height as usize {
+    //     let mut input_line = String::new();
+    //     io::stdin().read_line(&mut input_line).unwrap();
+    //     let line = input_line.trim_matches('\n').to_string();
+    // }
 
     // game loop
     loop {
